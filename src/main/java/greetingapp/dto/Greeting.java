a950d019174f26a452af5340bc1e594b7cd08ee0
Geek-Rajveer <120301212+Geek-Rajveer@users.noreply.github.com>
2025-02-22 15:19:41 +0530

    initial commit

---
+++ src/main/java/greetingapp/dto/Greeting.java
@@ -0,0 +1,14 @@
+package greetingapp.dto;
+
+public class Greeting {
+    private String name;
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
